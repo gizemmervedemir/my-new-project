b73b12a6f42899bb6bbb103df3845160be44b051
Gizem Merve Demir <gizemervedemir@gmail.com>
2025-02-24 11:44:06 +0300

    eslint

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -8,11 +8,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-400": "#F2A945",
-        "primary-500": "#F78410",
-        "primary-600": "#E07516",
-        background: "#ffffff",
-        foreground: "#000000",
+        primary: {
+          400: "#F2A945",
+          500: "#F78410",
+          600: "#E07516",
+        },
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
